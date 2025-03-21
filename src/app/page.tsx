f1e13315504cfb7952a55b828a267ab6e3bcf4b5
ibraSanusi <isanu227@gmail.com>
2025-03-21 16:50:05 +0100

    feat: ahora se redirecciona al dashboard o al login segun como este la sesion

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,13 @@
 // import { LatestPost } from "@/app/_components/post";
 import { HydrateClient } from "@/trpc/server";
+import { redirect } from "next/navigation";
+import { auth } from "@/server/auth";
 
 export default async function Home() {
+  const session = await auth();
+  if (!session?.user) redirect("/auth/login");
+
+  redirect("/dashboard");
   return (
     <HydrateClient>
       <h1>Pagina principal</h1>
