5d77ac6c7faa3fbeaa6f85781627a733e796fdb7
ibraSanusi <isanu227@gmail.com>
2025-05-15 11:42:05 +0200

    fix: quitado el tipado (da problemas)

--- src/server/db.ts
+++ src/server/db.ts
@@ -1,7 +1,6 @@
 import { PrismaClient } from "@prisma/client";
 
 import { env } from "@/env";
-import { type DefaultArgs } from "@prisma/client/runtime/library";
 
 const createPrismaClient = () =>
   new PrismaClient({
@@ -13,12 +12,6 @@ const globalForPrisma = globalThis as unknown as {
   prisma: ReturnType<typeof createPrismaClient> | undefined;
 };
 
-export const db: PrismaClient<
-  {
-    log: ("error" | "query" | "warn")[];
-  },
-  never,
-  DefaultArgs
-> = globalForPrisma.prisma ?? createPrismaClient();
+export const db = globalForPrisma.prisma ?? createPrismaClient();
 
 if (env.NODE_ENV !== "production") globalForPrisma.prisma = db;
