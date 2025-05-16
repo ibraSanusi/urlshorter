792f2d0b0f6f3c55226b7686e34859830b0f9ad4
ibraSanusi <isanu227@gmail.com>
2025-05-16 17:19:42 +0200

    fix: ajustado middleware para que no redirija aunque haya sesion

--- src/middleware.ts
+++ src/middleware.ts
@@ -2,7 +2,9 @@ import { NextResponse } from "next/server";
 import type { NextRequest } from "next/server";
 
 export function middleware(request: NextRequest) {
-  const isAuthenticated = request.cookies.get("authjs.session-token");
+  const isAuthenticated = request.cookies.get("authjs.session-token")?.value
+    ? true
+    : false;
 
   if (!isAuthenticated) {
     return NextResponse.redirect(new URL("/auth/login", request.url));
