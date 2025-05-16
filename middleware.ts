05616e566e747fa048a72e5bc19acf77c3013735
ibraSanusi <isanu227@gmail.com>
2025-05-16 03:45:34 +0200

    feature: protegidas ciertas rutas si no hay sesion

---
+++ middleware.ts
@@ -0,0 +1,17 @@
+import { NextResponse } from "next/server";
+import type { NextRequest } from "next/server";
+
+export function middleware(request: NextRequest) {
+  const isAuthenticated = request.cookies.get("authjs.session-token");
+
+  if (!isAuthenticated) {
+    return NextResponse.redirect(new URL("/auth/login", request.url));
+  }
+
+  return NextResponse.next();
+}
+
+// Solo proteger la ruta /chats
+export const config = {
+  matcher: ["/dashboard", "/dashboard/:path*", "/:path*"],
+};
